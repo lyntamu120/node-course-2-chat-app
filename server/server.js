84f6a20a9b1379a357324822a690ea290aed962e
王希宁 <xiningwang120@gmail.com>
2018-01-22 15:57:00 -0600

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat App',
+        createAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user has joined',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log("createMessage", message);
         io.emit('newMessage', {
@@ -28,8 +39,6 @@ io.on('connection', (socket) => {
         console.log('User was disconnected!');
     });
 });
-
-
 
 server.listen(port, () => {
     console.log(`server is up on ${port}...`);
